b00564c5ed40dfdc7f18b50d1f4c4fa4c326f6be
kust3n <sara.costa@iths.se>
2025-09-28 18:40:27 +0200

    Started scatterplot

--- Labs/Laboration 3/Laboration_3.py
+++ Labs/Laboration 3/Laboration_3.py
@@ -1,1 +1,22 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv("Labs/Laboration 3/unlabelled_data.csv", names=["x", "y"])
+
+plt.scatter(data["x"], data["y"])
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Unlabeled data:")
+
+k = 1
+m = 0
+
+x_vals = data['x']
+y_vals = k * x_vals + m
+
+plt.scatter(data['x'], data['y'])
+plt.plot(x_vals, y_vals, color='red', label=f'y = {k}x + {m}')
+plt.legend()
+
+plt.show()
 
